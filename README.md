1d24be35eecb94d46c1a3b28065f0301fa36fa21
Aaron Meese <ajmeese7@gmail.com>
2018-08-10 16:58:40 -0500

    Working properly
    Now I just need to go through and add options and make everything prettier

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # photo-gallery
-Displays all media from a folder
+Displays all media from a folder.
+
+### Usage
+In `index.php`, change the `subdomain` variable to a string containing the name
+of whatever subdomain you plan on deploying to on your server. If you aren't
+putting it in a subdomain or are just running it locally, you can skip this step
+and just leave it as an empty string.
+
+Put all the images you plan on using in a folder named `content`.
--- index.php
+++ index.php
@@ -4,32 +4,19 @@
     <title>Photo Gallery</title>
     <meta charset="UTF-8">
     <script type="text/javascript" src="jquery.min.js"></script>
-    <style>
-      img {
-        margin: 15px;
-      }
-
-      .square {
-        width: 15%;
-      }
-
-      .portrait {
-        width: 12.5%;
-      }
-
-      .landscape {
-        width: 20%;
-      }
-    </style>
+    <script type="text/javascript" src="masonry.min.js"></script>
+    <link rel="stylesheet" href="style.css"/>
   </head>
   <body>
     <h1>Welcome to my gallery!</h1>
+    <div class="grid" data-masonry='{ "itemSelector": ".grid-item" }'></div>
     <script>
+      // NOTE: Does NOT work if you use subdomain in front of address (i.e. gallery.mysite.com)
       var subdomain = "/photos"; // Write your subdomain here. Leave blank for local
 
       var images = <?php
         $images = array();
-        $folder = new DirectoryIterator("content");
+        $folder = new DirectoryIterator("content"); // name of media file
         foreach ($folder as $fileinfo) {
           if (!$fileinfo->isDot()) {
               $images[] = $fileinfo->getFilename();
@@ -41,21 +28,21 @@
       for (var i = 0; i < images.length; i++) {
         // TODO: Add support for other image types
         if (images[i].includes(".png") || images[i].includes(".jpeg") || images[i].includes(".gif") || images[i].includes(".jpg")) {
-          $("body").append( "<img id='" + i + "' src='" + subdomain + "/content/" + images[i] +"'>" );
+          // TODO: Remove jQuery? Is this the only reason I had it here?
+          var content = "<img class='grid-item' id='" + i + "' src='" + subdomain + "/content/" + images[i] +"'></div>";
+          $(".grid").append(content);
 
           var img = document.getElementById(i);
           var width = img.naturalWidth;
           var height = img.naturalHeight;
-          console.log("Image #" + i + ": [width: " + width + ", height: " + height + "]");
-          if (width > height) {
-            img.classList.add('landscape');
-          } else if (width == height) { // Square photo
-            img.classList.add('square');
-          } else {
+          if (width < height) { // portrait
             img.classList.add('portrait');
+          } else if (width == height) { // square
+            img.classList.add('square');
           }
         } else { // Assumes the media is a video
-          $("body").append("<video width='320' height='240' controls>" +
+          // IDEA: Add special video class support?
+          $("body").append("<video class='grid-item' width='320' height='240' controls>" +
           "<source src='/content/" + images[i] + "' type='video/mp4'>" +
           "Your browser does not support the video tag.</video>");
         }
